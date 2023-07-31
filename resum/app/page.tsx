46c299c6d108fd66a7e535f75d5906dedbdfd8f0
Igor Parfenov <lloopy123123543@gmail.com>
2023-07-31 18:20:03 +0500

    styles

--- resum/app/page.tsx
+++ resum/app/page.tsx
@@ -2,8 +2,9 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main>
-      div
+    <main className='w-full h-10 '>
+      <div>
+      </div>
     </main>
   )
 }
